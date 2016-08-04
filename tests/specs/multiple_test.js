82682c42256d69387f0dae0548db6bac3146bc0f
Vibhaj Rajan <vibhaj8@gmail.com>
2016-08-05 01:34:12 +0530

    added example for multiple specs in single session

--- tests/specs/multiple_test.js
+++ tests/specs/multiple_test.js
@@ -1,28 +1,11 @@
 var assert = require('assert');
 
-describe('Google\'s Search Functionality', function() {
-  it('opens google', function () {
-    browser
-      .url('https://www.google.com/ncr')
-      
-    assert(browser.getTitle().match(/Google/i));
-  });
+var specs = [
+  './multiple/test_01.js',
+  './multiple/test_02.js',
+  './multiple/test_03.js'
+];
 
-  it('can find search results', function () {
-    browser
-      .url('https://www.google.com/ncr')
-      .setValue('*[name="q"]','BrowserStack\n')
-      .pause(5000);
-    
-    assert(browser.getTitle().match(/BrowserStack - Google Search/i));
-  });
-
-  it('can find search results for non ncr', function () {
-    browser
-      .url('https://www.google.com')
-      .setValue('*[name="q"]','BrowserStack\n')
-      .pause(5000);
-    
-    assert(browser.getTitle().match(/BrowserStack - Google/i));
-  });
-});
+for (var i = specs.length - 1; i >= 0; i--) {
+  require(specs[i]);
+};
