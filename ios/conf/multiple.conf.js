2e1ec77247cf7a9b388b64a58d9f8722218354a1
Hani <Hani@Hanis-MBP.local>
2018-04-05 18:10:20 +0530

    Setting default access key length to 20
    - WebDriver IO determines the cloud provider by the length of access key
    - In case the env variable BROWSERSTACK_ACCESS_KEY is empty, we set it to
    string BROWSERSTACK_ACC_KEY which is 20 characters
    
    Goal: To avoid confusing error message in case the env variable is empty

--- ios/conf/multiple.conf.js
+++ ios/conf/multiple.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
-  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACC_KEY',
 
   updateJob: false,
   specs: [
